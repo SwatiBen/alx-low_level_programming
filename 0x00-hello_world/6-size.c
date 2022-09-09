1db575c3087a45ca771e1e65ab78ba22a239f10b
Swati Beniwal <swatibeniwal95@gmail.com>
2022-09-08 19:21:04 -0700

    hi

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+	pritnf("Size of a char: %d byte(s)\n", sizeof(char));
+	pritnf("Size of an int: %d byte(s)\n", sizeof(int));
+	pritnf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	pritnf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	pritnf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
+}
+
+
