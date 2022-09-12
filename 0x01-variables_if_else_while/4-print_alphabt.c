7ba13d7c3ab2123c73cc3618b772a5fa9bc94d1e
Swati Beniwal <swatibeniwal95@gmail.com>
2022-09-12 08:21:12 -0700

    hi

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *  * main - print if the number is positive, zero, or negative
+ *  	*Description: theis is it
+ *  	    * this is the program to print
+ *  	         Return: 0
+ *  	         */
+int main(void)
+{ 
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+			if (ch != 'e' && ch != 'q')
+			{
+				purchar(ch);
+			}
+		}
+		putchar('\n');
+	return(0);
+	}
