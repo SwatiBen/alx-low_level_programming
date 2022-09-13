f306c4c57a7826d915476e4fbbaf86fbfd29a93f
Swati Beniwal <swatibeniwal95@gmail.com>
2022-09-12 19:43:13 -0700

    i

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-/** 
+/* * 
  *  * main - print the no.
  *   *
  *    * Description: we are using the main here
  *     * print the program
  *      * Return: 0
- *       */
+ */
 int main(void)
 {
 	char c;
-	for(c= '0'; c <= '9'; c++)
+
+	for (c = '0'; c <= '9'; c++)
 	{
 		putchar(c);
 	}
