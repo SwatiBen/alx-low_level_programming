4e255f1a0cc6d49cc05038a84e4671703b7235cd
Swati Beniwal <swatibeniwal95@gmail.com>
2022-09-12 19:52:06 -0700

    g

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+ *  main - ples this nope
+ *  Description: main function is used
+ *  thsi is the printing progran
+ *  Return: 0
+ */
+int main(void)
+{
+	char c;
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar(c);
+	return(0);
+}
+
