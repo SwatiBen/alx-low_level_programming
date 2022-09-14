ad9c1cd5a9488f85d03f54914a4695c5f8c63acd
Swati Beniwal <swatibeniwal95@gmail.com>
2022-09-14 13:16:02 -0700

    k

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *  print_sign - name of function
+ *  Description: character is checked
+ *  @w: number
+ *  Return: 0
+ */
+
+int print_sign(int w)
+{
+	if (w > 0)
+	{
+		_putchar('+';
+		return (1);
+	}
+	else if (w == 0)
+	{
+		_putchar('0');
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
