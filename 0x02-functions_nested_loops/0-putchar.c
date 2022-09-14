53b326440499292d208dbd83eb1dc9c69a335685
Swati Beniwal <swatibeniwal95@gmail.com>
2022-09-14 10:10:46 -0700

    k

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /*
- *  main: program entry point
+ *  main - program entry point
  *  Description: putchar is printed with a new line
  *  Return: 0
  */
 
 int main(void)
-{ 
+{
 	char mychar[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
 	int i = 0;
 
