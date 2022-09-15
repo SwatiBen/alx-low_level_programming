8a5e15535e949e8d17d184fbfd8a9e768ba7004b
Swati Beniwal <swatibeniwal95@gmail.com>
2022-09-14 19:35:16 -0700

    k

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *  _abs - name of function
+ *  Description: calculate the absolute value
+ *  @w: convert number
+ *  Return: 0
+ */
+
+int _abs(int w)
+{
+	if (w < 0)
+		return (w * -1);
+	else
+		return (w);
+}
