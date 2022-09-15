7dfde88239d9eb16f48c68eebb2dadb596e652e2
Swati Beniwal <swatibeniwal95@gmail.com>
2022-09-14 19:59:18 -0700

    l

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -24,8 +24,8 @@ void jack_bauer(void)
 					_putchar(b);
 					_putchar(58);
 					_putchar(c);
-				        _putchar(d);
-				        _putchar('\n');
+					_putchar(d);
+					_putchar('\n');
 					}
 			}
 		}
