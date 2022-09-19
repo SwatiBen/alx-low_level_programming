2c81542b58464c1eb8404cc4ce8c562ec3617c51
Swati Beniwal <swatibeniwal95@gmail.com>
2022-09-19 12:40:24 -0700

    k

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_rev - print a string
+ * @s: string print
+ * Description: pritn reverse string
+ */
+void print_rev(char *s)
+{
+	int a, b, len;
+
+	a = 0;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+
+	len = a;
+
+	for (b = len - 1; b >= 0; b--)
+	{
+		_putchar(s[b]);
+	}
+
+	_putchar('\n);
+}
