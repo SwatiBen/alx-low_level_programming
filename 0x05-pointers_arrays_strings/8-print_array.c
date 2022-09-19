d4c6aaa4d6951eb83b961796c705a9511b5614c7
Swati Beniwal <swatibeniwal95@gmail.com>
2022-09-19 11:57:25 -0700

    l

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array - print elements
+ * Description: print the array of intergers
+ * @a: array to print
+ * @n: elemfent to print
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for i = 0; i < n; i++)
+	{
+		if (i == 0)
+			printf("%d", a[i]);
+		else
+			printf(", %d", a[i]);
+	}
+		printf("\n");
+}
