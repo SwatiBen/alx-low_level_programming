8b185d7c713addb261918a1435a619f458d121ee
Swati Beniwal <swatibeniwal95@gmail.com>
2022-09-22 09:20:20 -0700

    j

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
  * reverse_array - function
  * Description: return the fun as eitner 0 or 1
  * @a: integer
  * @n: t nmver
  */
+
 void reverse_array(int *a, int n)
 {
 	int b = 0, c, d, e;
