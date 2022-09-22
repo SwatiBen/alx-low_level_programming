da1a84f0f86366e515e1c2974497ccf7e2bf96ae
Swati Beniwal <swatibeniwal95@gmail.com>
2022-09-22 04:06:23 -0700

    k

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcmp - function
+ * @s1: th chain
+ * @s2: chaine
+ * Description: the returnthe value
+ * Return: 1 or 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' && (*s2 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	if (s1 == s2)
+		return (0);
+	else
+		return ((int)*s1 - (int)*s2);
+}
