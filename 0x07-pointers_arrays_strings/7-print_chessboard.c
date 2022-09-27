20bb7977c5be641fb17548ebb89903f923a4a2c2
Swati Beniwal <swatibeniwal95@gmail.com>
2022-09-26 21:31:52 -0700

    l

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_chessboard - print
+ * @a: array
+ */
+void print_chessboard(char (*a)[8])
+{
+	int b, c;
+
+	for (b = 0; b < 8; b++)
+	{
+		for (c = 0; c < 8; c++)
+		{
+			putchar(a[b][c];
+		}
+		putchar('\n');
+	}
+}
+
