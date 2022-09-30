fc3bfacf74150f38a2a0baf45ef56273b11947db
Swati Beniwal <swatibeniwal95@gmail.com>
2022-09-29 19:31:54 -0700

    l

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * _sqrt - function
- * @n: the num
- * @i: num
+ * _sqrt_recursion - main fun
+ * @n: int n
  * Return: 0
  */
-int _sqrt(int n, int i )
+int _sqrt_recursion(int n)
 {
-	if (i * i > n)
+	return (_sqrt(n, 1));
+}
+/**
+ * _sqrt - fun
+ * @n: integer
+ * @i: int
+ * Return: int
+ */
+int _sqrt(int n, int i)
+{
+	if (n < 0)
+		return (-1);
+	if ((i *i) > n)
 		return (-1);
 	if (i * i == n)
 		return (i);
-	else
-		return (_sqrt(n, i + 1));
+	return (_sqrt(n, i + 1));
 }
