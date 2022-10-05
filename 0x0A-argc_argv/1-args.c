77436925943f635dc07ce3b2fe0ca1dc64015400
Swati Beniwal <swatibeniwal95@gmail.com>
2022-10-04 19:11:54 -0700

    ;l

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - fun
+ * @argc: it is
+ * @argv: argument aray
+ */
+int main(int argc, char **argv _attribute_((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
