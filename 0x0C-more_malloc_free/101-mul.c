7473a8760e68c688f8f7552a1df9538493ee450c
Swati Beniwal <swatibeniwal95@gmail.com>
2022-10-06 04:54:49 -0700

    ;

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,5 @@
-#include <stdio.h>
 #include "main.h"
-#include <stdlib.h>
+
 /**
  * main - fun
  * @argc: argument
@@ -11,20 +10,21 @@ int main(int argc, char *argv[])
 {
 	unsigned long m;
 	int a, b;
-	
+
 	if (argc != 3)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+
 	for (a = 1; a < argc; a++)
 	{
 		for (b = 0; argv[a][b] != '\0'; b++)
 		{
 			if (argv[a][b] > 57 || argv[a][b] < 48)
 			{
-			printf("Error\n");
-			exit(98);
+				printf("Error\n");
+				exit(98);
 			}
 		}
 	}
