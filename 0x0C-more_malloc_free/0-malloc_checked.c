e2215d8487738928ceff920d9204928b3828491f
Swati Beniwal <swatibeniwal95@gmail.com>
2022-10-07 19:25:32 -0700

    ;

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - fun
+ * @b: var
+ * Return: 0
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *a = malloc(b);
+
+	if (a ==NULL)
+		exit(98);
+	return(a);
+}
