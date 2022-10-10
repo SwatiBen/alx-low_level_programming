ba172a17cb22dd10c972117f1c702b85b068e4eb
Swati Beniwal <swatibeniwal95@gmail.com>
2022-10-10 05:02:04 -0700

    ;

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+/**
+ * init_dog - fun of struct dog
+ * @d: struct dog
+ * @name: dog's name
+ * @age: age
+ * @owner: owns dog
+ * Return: none
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->owner = owner;
+		d->age = age;
+	}
+}
