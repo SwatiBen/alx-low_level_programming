2ccf8f612659f0bf64235b5ecec640bfe332ba1d
Swati Beniwal <swatibeniwal95@gmail.com>
2022-10-10 18:11:41 -0700

    ;

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * free_dog - a fun fo rdog a
+ * @d: dog st
+ * Return: none
+ */
+void free_dog(dog_t * d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
