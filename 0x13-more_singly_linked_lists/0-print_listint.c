52d2187e9d78916f9e21a34f9d2352936854d6a3
Swati Beniwal <swatibeniwal95@gmail.com>
2022-10-25 11:11:23 -0700

    ;

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,5 @@
 #include "lists.h"
-#include <stdio.h>
+
 /**
  * print_listint - linked list elemetn got printed
  * @n: the nnom
@@ -8,15 +8,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	int a = 0;
+	size_t a = 0;
 
-	if (h != NULL)
+	while (h != NULL)
 	{
-		while (h)
-		{
-			printf("%d\n", h->n);
-			h = h->a;
-			a++;
+		printf("%d\n", h->n);
+		h = h->a;
+		a++;
 		}
 	}
 	return (a);
